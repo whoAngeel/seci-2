dfabdb09cd143ace1ca271e2ae87ed4c44feaf59
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-06-05 01:45:45 -0600

    vista de la tabla de hoy

---
+++ components/ui/tableToday.vue
@@ -0,0 +1,37 @@
+<template>
+  <table class="table table-xs w-1/3 ">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Hombres</th>
+          <th>Mujeres</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="carrera in recordToday.carreras" :key="carrera.id" class="hover">
+          <td class="uppercase ">{{ carrera.nombre }}</td>
+          <td class="slashed-zero tabular-nums">
+              {{ carrera.hombres }}
+          </td>
+          <td class="slashed-zero tabular-nums  font-medium">
+            {{ carrera.mujeres }}
+          </td>
+          <td class="slashed-zero tabular-nums">
+            <span class=" font-bold text-sm">
+              {{ carrera.total }}
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+</template>
+
+<script setup>
+const props = defineProps(['recordToday'])
+</script>
+
+<style>
+
+</style>
--- pages/registers.vue
+++ pages/registers.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
-    registros
+  <div class="flex  h-full w-full items-center  flex-col rounded-2xl static ">
+    <h2 class="text-2xl absolute top-16  ">
+      <p class="bg-purple-400 text-zinc-800 p-6 py-7 rounded-lg">
+        Carreras de Hoy - <span class="font-bold ">{{ recordToday?.date }}</span>
+      </p>
+    </h2>
+    <div class="mt-6 bg-base-300 p-6 rounded-lg absolute top-28 z-10" >
+      <tableToday :recordToday="recordToday"/>
+      <p class="text-right text-lg font-medium mr-4 mt-2">Total: <span class="badge badge-primary badge-md">{{ totalDia }}</span></p>
+    </div>
+
   </div>
 </template>
 
-<script lang="ts" setup>
+<script setup>
+import tableToday from '../components/ui/tableToday.vue';
+
+
+const {recordToday, carreras, totalDia} = useMyRecordStore()
 
 </script>
 
-<style>
-
-</style>
+<style></style>
