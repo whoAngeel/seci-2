8d24637a4dc4f04aa374da24d7e12df5ddecbf72
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-06-17 06:37:57 -0600

    Validando el boton de exportar

--- pages/savereports.vue
+++ pages/savereports.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="w-full h-full static">
-    <p class="absolute top-5 text-2xl font-semibold  text-center">Descargar registros</p>
+    <p class="absolute top-5 text-2xl font-semibold  text-center">Generar reporte</p>
 
     <div class="flex justify-evenly">
       <div class="flex gap-2">
         <div class="flex gap-2 items-center justify-between">
+          <div class="tooltip " data-tip="Limpiar">  
+            <button @click="cleanDates" class="btn btn-square btn-sm" v-if="startDate || endDate">
+              <Icon name="icon-park-solid:clear-format" />
+            </button>
+          </div>
           <span>Fecha inicial:</span>
           <select v-model="startDate" @change="updateEndDate" class="select select-accent select-sm "
             aria-placeholder="Tef">
@@ -13,17 +18,25 @@
           </select>
         </div>
 
-        <div class="flex gap-2 items-center justify-between" v-if="enableEndDate">
+        <div class="flex gap-2 items-center justify-between" v-if="enableEndDate || endDate">
           <span>Fecha final:</span>
           <select v-model="endDate" class="select select-accent select-sm">
             <option disabled value="">Seleccionar fecha final</option>
             <option v-for="date in fechasDisponibles" :key="date" :value="date">{{ date }}</option>
           </select>
+          <div class="tooltip " data-tip="Recargar">  
+            <button class="btn btn-square btn-sm" v-if="endDate">
+              <Icon name="material-symbols:directory-sync" />
+            </button>
+          </div>
         </div>
+        
 
         <!-- <input type="date" placeholder="Type here" class="input input-bordered input-success input-sm " /> -->
       </div>
-      <button class='btn btn-sm btn-primary disabled:btn-disabled' :disabled="!enableExportButton">Exportar</button>
+      <button class='btn btn-sm btn-primary disabled:btn-disabled' :disabled="!enableExportButton || !validButton">
+        <Icon name="material-symbols:download-rounded"/>
+        Exportar</button>
     </div>
 
     <!--  Registros -->
@@ -40,20 +53,26 @@ const store = useRecordsStore()
 
 const startDate = ref(null)
 const endDate = ref(null)
-const validDate = ref(true)
+const validButton = ref(true)
 const enableEndDate = ref(false)
 
 const fechasDisponibles = computed(() => {
   if (startDate.value && store.dates.length > 0) {
     const fechaInicialIndex = store.dates.indexOf(startDate.value);
     if (fechaInicialIndex !== -1) {
-      return store.dates.slice(0, fechaInicialIndex+1)
+      return store.dates.slice(0, fechaInicialIndex + 1)
     }
   }
   return [];
 });
 
-const enableExportButton = computed(()=>{
+const cleanDates=()=>{
+  startDate.value = null
+  endDate.value = null
+  validButton.value = false
+}
+
+const enableExportButton = computed(() => {
   if (startDate.value && endDate.value) {
     return true; // Ambas fechas seleccionadas
   } else if (startDate.value && !endDate.value) {
@@ -65,6 +84,7 @@ const enableExportButton = computed(()=>{
 
 const updateEndDate = () => {
   enableEndDate.value = true;
+  // return enableEndDate.value || endDate.value!=null
 };
 
 onMounted(async () => {
